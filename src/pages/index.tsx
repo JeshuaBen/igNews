cfb30563d6e21fd47db56bf005b3ea2343acbd8d
Jeshua Costa <jeshuacostafer@gmail.com>
2022-06-20 12:18:54 -0300

    created a global style and the document file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
-import style from "../styles/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
-  return <h1 className={style.title}>Hello World</h1>;
+  return (
+    <>
+      <Head>
+        <title>Início | igNews</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  );
 }

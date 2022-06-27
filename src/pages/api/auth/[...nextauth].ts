4edd67447887c1062529129cf7d077cba0124a73
Jeshua Costa <jeshuacostafer@gmail.com>
2022-06-27 20:28:01 -0300

    Inserted data on the FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -13,4 +17,26 @@ export default NextAuth({
       },
     }),
   ],
+
+  secret: process.env.JWT_SECRET,
+
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const userEmail = user.email;
+
+      try {
+        await fauna.query(
+          q.Create(
+            // Onde Collection são as 'tabelas'
+            q.Collection("users"),
+            // Objeto que contém os dados que nós queremos inserir
+            { data: { userEmail } }
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
